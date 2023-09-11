19fc48c2ab18c6dd70ff2c4c571e4d4a10f007c8
oziltharwat <144340129+oziltharwat@users.noreply.github.com>
2023-09-11 16:14:31 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,96 @@
+
+
+
+
+
+// var fristName = window.prompt()
+
+// if (fristName == "ozil") {
+//     console.log("ozil");
+// }
+// else if (fristName == "اوزيل") { console.log("اوزيل"); }
+
+// else {
+//     console.log("lafjlfkajlfjksd");
+// }
+
+
+
+// var cartona = i
+// for (var i = 0; i < 100; i++) {
+
+//     cartona +=
+//         ` <div class="col-md-3 bg-info-subtle ">
+//  <h1>Lorem ipsu</h1>
+//  <img class="w-100" src="photos/1.jpg" alt="">
+//  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil ut quis, magni quae dicta ea debitis recusandae excepturi placeat maiores cumque tempore ducimus ex pariatur quod, commodi aliquam eaque? Praesentium!</p>
+// </div>`
+
+//     document.getElementById("demo").innerHTML = cartona
+// }
+
+
+
+
+// var fa = window.prompt()
+
+// if (prise => 700) {
+//     let mag = "prise"
+
+//     document.getElementById("mimo").innerHTML =  mag + prise
+// }
+// else if (Name == "") { console.log(""); }
+
+// else {
+//     console.log("");
+// }
+
+
+// function getprise(x, y) {
+//     let prise = x + y
+//     let newprise = x + y * 2
+
+
+//     console.log(prise);
+//     console.log(newprise);
+
+// }
+
+// getprise(10, 20)
+
+
+
+
+// let all ={
+//     name:"ozil" ,
+//     age:25,
+//     waife:{
+//         name:"nada",
+//         age:22
+//     }
+// }
+// console.log(all.waife);
+
+
+
+// let prices = {
+//     pastaprice: 20,
+//     mahshyprice: 25,
+
+//     eat: function () {
+//         console.log(pastaprice + mahshyprice);
+//     }
+//     prices.eat()
+// }
+
+// let ayhga = ["mohamed", "mohamed", "mohamed", "mohamed", "mohamed", "mohamed"]
+// for (let i = 0; i < ayhga.length; i++) {console.log(ayhga[i]);
+
+// }
+
+
+function btn(params) {
+    
+    console.log("coder");
+    
+} 
